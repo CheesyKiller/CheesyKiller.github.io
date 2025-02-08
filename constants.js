a8e0dfe1b9f6483d7c703e864ba669813ee12eff
Cheese <112054532+CheesyKiller@users.noreply.github.com>
2025-02-08 02:25:55 -0800

    Got webpages to load on my local after generation. Pushing currently but the build should almost be ready to be served. Just need to finish alterations and upgrades from the website I made for my friend.

--- constants.js
+++ constants.js
@@ -1,5 +1,13 @@
-const strings = {
-    resumeName: "Vinny"
+export const strings = {
+    resumeName: "Jacob Abts",
+    index: "Projects",
+    about: "Resume"
 }
 
-export default strings;
+export const colors = {
+    buttonBackground: '#f0f0f0',
+    border: '#000000',
+    background: '#acacac',
+    importantText: '#0F3489',
+    text: '#000000'
+}
